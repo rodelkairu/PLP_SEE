2b4b6d4efb28c4a188a9dd9e29279e1fa647240c
rodelkairu <samuelkarubu18@gmail.com>
2025-07-17 11:31:07 +0300

    Add calc.py and classX.py

---
+++ calc.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+addition = num1 + num2
+print("The sum is:", addition)
+subtraction =  num1 - num2
+print(f"The difference is: {subtraction}")
+multiplication = num1 * num2
+print("The product is:", multiplication)
+if num2 != 0:
+    division = num1 / num2
+    print("The quotient is:", division)
+else:
+    print("Cannot divide by zero!")
+# 🎉 You've just created a mini-calculator! Keep having fun with your coding    !
